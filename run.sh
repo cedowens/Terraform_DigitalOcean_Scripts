46169432d0f184d9cc4ba9d25f709d7c684efa7a
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-01-01 12:07:47 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ read keyName
 echo "=====>Enter the path to where the ssh private key is that you use to ssh into Digital Ocean"
 read keyPath
 
-cd DO_new_ubuntu_droplet_2
+cd DO_new_ubuntu_droplet_with_firewall
 
 sed -i -e "s/myc2-1/$dropletName/g" droplet-config.tf
 sed -i -e "s/myc2rule/$firewallName/g" droplet-config.tf
